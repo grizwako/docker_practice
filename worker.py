69d3cc98135d915f76a6cbd32af5bf77d9022e29
grizwako <grizwako@gmail.com>
2018-02-01 22:20:13 +0100

    Worker publishes message about user's favorite number being saved.

--- worker.py
+++ worker.py
@@ -4,20 +4,22 @@ import asyncio
 import aioredis
 
 redis = None
+publisher = None
 
 
 async def connect():
-    global redis
-    redis = await aioredis.create_redis(
-        'redis://localhost',
-        loop=loop)
+    global redis, publisher
+    redis = await aioredis.create_redis('redis://localhost')
+    publisher = await aioredis.create_redis('redis://localhost')
 
 
 async def process_tasks():
     while True:
         msg = await redis.brpop('tasks', timeout=0)
-        user, number = msg[1].decode('utf-8').split(':')
+        decoded_msg = msg[1].decode('utf-8')
+        user, number = decoded_msg.split(':')
         await redis.set('user:'+user, number)
+        await publisher.publish('user_saved', decoded_msg)
 
 
 if __name__ == '__main__':
